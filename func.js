0715f67411312cbd7566fa81af918c26b3025a99
leonovvavictorria <78730431+leonovvavictorria@users.noreply.github.com>
2022-06-08 17:23:38 +0300

    ✨ add anims

---
+++ func.js
@@ -0,0 +1,11 @@
+const loves = [...document.getElementsByClassName('possion2')]
+console.log(loves)
+function initElement() {
+    i ^= 1
+    image.style.display = s[i];
+    setTimeout(initElement, t[i]);
+}
+loves[0].onclick = () => { loves[0].style.opacity = 1 }
+loves[1].onclick = () => { loves[1].style.opacity = 1 }
+loves[2].onclick = () => { loves[2].style.opacity = 1 }
+loves[3].onclick = () => { loves[3].style.opacity = 1 }
